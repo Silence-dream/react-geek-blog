01d32e7135b398fe47b9071227a68a322d386e75
silence_dream <1428482231@qq.com>
2022-04-20 10:26:34 +0800

    init2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 const { resolve } = require('path');
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
