de87e98497e4e925a8dec39b59fcebce8fca4352
silence_dream <1428482231@qq.com>
2022-04-21 10:14:23 +0800

    封装http

--- src/store/actions/user.ts
+++ src/store/actions/user.ts
@@ -1,17 +1,20 @@
-import axios from 'axios';
-
 import { setToken } from '@/utils';
+import { http } from '@/utils';
 
 export const login = (mobile: string, code: string) => {
-  console.log(111);
   return async (dispatch: any) => {
-    const res = await axios.post('http://geek.itheima.net/v1_0/authorizations', {
-      mobile,
-      code,
-    });
+    // https://docs.shanyuhai.top/frontend/typescript/change-axios-response-type.html
+    const res = await http.post<any, { token: string; refresh_token: string }>(
+      '/authorizations',
+      {
+        mobile,
+        code,
+      },
+    );
+
+    // console.log(res.token);
+    let { token } = res;
     console.log(res);
-    // 注意：此处获取的是 token
-    const { token } = res.data.data;
     setToken(token);
     dispatch({ type: 'user/setToken', payload: token });
   };
